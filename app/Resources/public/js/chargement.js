21c1fcb711290c585e8b6ebfde93039b9572ca7f
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-06 23:24:58 +0100

    chargement optimisé

--- app/Resources/public/js/chargement.js
+++ app/Resources/public/js/chargement.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
+    $('#chargement').fadeTo(200, 0, function() {
+        $('#chargement').css('visibility', 'hidden');
+    });
+
+    $('#content').fadeIn(200);
+
     $('a').click(function() {
         $('#content').fadeOut(200);
 
         $('#chargement').css('visibility', 'visible');
         $('#chargement').fadeTo(200, 1);
     });
-});
-
-$(window).load(function() {
-    $('#chargement').fadeTo(200, 0, function() {
-        $('#chargement').css('visibility', 'hidden');
-    });
-
-    $('#content').fadeIn(200);
 });
