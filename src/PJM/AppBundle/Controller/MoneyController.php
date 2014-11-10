66a36ce43b6276c3c6de8ca1d337cfee22bb8095
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-10 16:56:12 +0100

    continuer money

--- src/PJM/AppBundle/Controller/MoneyController.php
+++ src/PJM/AppBundle/Controller/MoneyController.php
@@ -9,20 +9,39 @@ class MoneyController extends Controller
     public function envoyerPaiementAction()
     {
         $buzz = $this->container->get('buzz');
-        $url = 'https://rest.s-money.fr/commerce/payments/smoney';
-        $response = $buzz->post($url, array(), array(
-            "amount" => 20,
-        ));
+        $curl = $buzz->getClient();
+        $curl->setOption(CURLOPT_SSL_VERIFYHOST, false);
+        $curl->setOption(CURLOPT_SSL_VERIFYPEER, false);
+
+        $url = 'https://rest-pp.s-money.fr/commerce/payments/smoney';
+        // #FUTURE https://rest.s-money.fr/commerce/payments/smoney
+        $headers = array(
+            "Authorization" => "Bearer ***REMOVED***",
+        );
+        $content = array(
+            "amount" => 0,
+            "receiver" => "aeensambordeaux",
+            "transactionId" => "0",
+            "amountEditable" => true,
+            "receiverEditable" => true,
+            "agent" => "other",
+            "source" => "web",
+            "identifier" => ""
+        );
+
+        // post(url, headers, content)
+        $response = $buzz->post($url, $headers, $content);
 
         if ($response->getStatusCode() != 200) {
-            // handle failure
+            // échec
         }
 
         $content = $response->getContent();
         $data = json_decode($content);
 
         return $this->render('PJMAppBundle:Money:index.html.twig', array(
-            'response' => $data
+            'response' => $response,
+            'requete' => $buzz->getLastRequest()
         ));
     }
 }
