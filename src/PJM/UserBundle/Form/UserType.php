f1e65751b1615dc3a6e021cb0dd5e31c6e7553f7
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-11 15:28:44 +0100

    disable registration

---
+++ src/PJM/UserBundle/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PJM\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('bucque')
+            ->add('email')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PJM\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'pjm_userbundle_user';
+    }
+}
