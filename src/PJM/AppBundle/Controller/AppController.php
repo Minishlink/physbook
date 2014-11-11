f0dedf2efda9c7f25754110bf619ece83a89593e
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-11 23:48:34 +0100

    Page Contact

--- src/PJM/AppBundle/Controller/AppController.php
+++ src/PJM/AppBundle/Controller/AppController.php
@@ -3,6 +3,9 @@
 namespace PJM\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class AppController extends Controller
 {
@@ -14,5 +17,47 @@ class AppController extends Controller
     public function aProposAction()
     {
         return $this->render('PJMAppBundle:App:a_propos.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $envoi = false;
+        $user = $this->getUser();
+
+        $form = $this->createFormBuilder()
+            ->add('sujet', 'text', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('min' => 3)),
+                ),
+            ))
+            ->add('contenu', 'textarea', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('min' => 20)),
+                ),
+            ))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject("[Physbook] [Contact] ".$data['sujet'])
+                ->setFrom(array($user->getEmail() => $user->getUsername()))
+                ->setTo(array('louis.lagrange@gadz.org' => 'Louis Lagrange'))
+                ->setBody($data['contenu'])
+            ;
+            $this->get('mailer')->send($message);
+
+            $envoi = true;
+        }
+
+        return $this->render('PJMAppBundle:App:contact.html.twig', array(
+            'form' => $form->createView(),
+            'envoi' => $envoi
+        ));
     }
 }
