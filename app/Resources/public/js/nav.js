912baca09509a07848a2131a5c673dfdf0ac4061
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-12 18:01:44 +0100

    header debut sous-menu

--- app/Resources/public/js/nav.js
+++ app/Resources/public/js/nav.js
@@ -7,20 +7,41 @@ $(document).ready(function () {
     $('ul.enable-slider li a').hover(
         function() {
             var left = $(this).parent().position().left;
-            var width = $(this).parent().width();
+            var width = $(this).parent().width()+1;
+            var target = $(this).attr('data-target');
+
+            switch(target) {
+                case '#menu-vie':
+                    $('#nav-slider').css('background-color', '#b63938');
+                    break;
+                case '#menu-assos':
+                    $('#nav-slider').css('background-color', '#febf00');
+                    break;
+                case '#menu-consos':
+                    $('#nav-slider').css('background-color', '#eb661d');
+                    break;
+                case '#menu-thuyss':
+                    $('#nav-slider').css('background-color', '#6a8fda');
+                    break;
+                case '#menu-tutos':
+                    $('#nav-slider').css('background-color', '#804faa');
+                    break;
+            }
 
             $('#nav-slider').stop().animate({
                 'left' : left,
-                'width' : width
+                'width' : width,
+                'opacity': 100
             });
         },
         function() {
-            var left = $('ul.enable-slider li.active').parent().position().left;
-            var width = $('ul.enable-slider li.active').width();
+            var left = $('ul.enable-slider li.active').position().left;
+            var width = $('ul.enable-slider li.active').width()+1;
 
             $('#nav-slider').stop().animate({
                 'left' : left,
-                'width' : width
+                'width' : width,
+                'opacity': 0
             });
         }
     );
@@ -40,8 +61,8 @@ $(document).ready(function () {
 
 
 function initSlider() {
-    var left = $('ul.enable-slider li.active').parent().position().left;
-    var width = $('ul.enable-slider li.active').width();
+    var left = $('ul.enable-slider li.active').position().left;
+    var width = $('ul.enable-slider li.active').width()+1;
 
     $('#nav-slider').css({'left' : left, 'width' : width});
 }
