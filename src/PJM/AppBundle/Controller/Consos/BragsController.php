679412ff4f90b9b08d0efeb1baf5a25769d92ffc
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-14 12:27:58 +0100

    debut formulaire rechargement s-money brag's

--- src/PJM/AppBundle/Controller/Consos/BragsController.php
+++ src/PJM/AppBundle/Controller/Consos/BragsController.php
@@ -9,10 +9,42 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class BragsController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $resHandleFormRechargement = $this->handleFormRechargement($request);
+        $formRechargement = $resHandleFormRechargement['form'];
+        $message = $resHandleFormRechargement['message'];
+
         return $this->render('PJMAppBundle:Consos:brags.html.twig', array(
-            'erreur' => isset($erreur) ? $erreur : null
+            'formRechargement' => $formRechargement->createView(),
+            'message' => isset($message) ? $message : null
         ));
+    }
+
+    private function handleFormRechargement(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('montant', 'money', array(
+                'constraints' => array(
+                new NotBlank(),
+                ),
+            ))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            $message = array(
+                'niveau' => 'success',
+                'contenu' => 'Rechargement de '.$data['montant'].'€ effectué.'
+            );
+        }
+
+        return array(
+            'form' => $form,
+            'message' => isset($message) ? $message : null
+        );
     }
 }
