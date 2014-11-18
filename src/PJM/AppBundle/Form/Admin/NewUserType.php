de9da18d322c788f0c3922d1b7766d733c08486d
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-18 02:25:24 +0100

    admin + new user

---
+++ src/PJM/AppBundle/Form/Admin/NewUserType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace PJM\AppBundle\Form\Admin;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class NewUserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', 'email')
+            ->add('fams', 'text')
+            ->add('tabagns', 'choice', array(
+                'choices' => array(
+                    "bo" => "Bordel's",
+                    "li" => "Birse",
+                    "an" => "Boquette",
+                    "me" => "Siber's",
+                    "ch" => "Chalon's",
+                    "cl" => "Clun's",
+                    "ai" => "KIN",
+                    "ka" => "Kanak",
+                    "pa" => "Paris",
+                )))
+            ->add('proms', 'text')
+            ->add('bucque', 'text')
+            ->add('prenom', 'text')
+            ->add('nom', 'text')
+            ->add('telephone', 'text', array('required' => false))
+            ->add('appartement', 'text', array('required' => false))
+            ->add('classe', 'text', array('required' => false))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PJM\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'pjm_appbundle_admin_new_user_form';
+    }
+}
