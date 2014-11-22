9d0f981bf44d15fb4d549f611df6dff65edadb5c
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-22 18:57:40 +0100

    price filter

--- src/PJM/AppBundle/Twig/IntranetExtension.php
+++ src/PJM/AppBundle/Twig/IntranetExtension.php
@@ -14,8 +14,10 @@ class IntranetExtension extends \Twig_Extension
     public function prixFilter($string)
     {
         $unit = "c";
+        $price = (float) $string / 100; // centimes => euros
         if (strlen($string) > 2 || $string == "0") {
             $unit = "€";
+            $string = number_format($price, 2, ',', ' ');
         }
 
         return $string.$unit;
