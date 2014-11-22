cd5f8f87b38b10248999c9811ac4314f757d6e6a
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-22 17:45:15 +0100

    modifier la commande de pain

--- src/PJM/AppBundle/Controller/Consos/BragsController.php
+++ src/PJM/AppBundle/Controller/Consos/BragsController.php
@@ -7,11 +7,24 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\Range;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+use PJM\AppBundle\Entity\Historique;
+use PJM\AppBundle\Form\Consos\CommandeType;
+
 class BragsController extends Controller
 {
+    private $slug;
+
+    public function __construct()
+    {
+        $this->slug = 'brags';
+    }
+
     public function indexAction(Request $request, $messages = null)
     {
-        $slug = 'brags';
+        $em = $this->getDoctrine()->getManager();
+        $prixBaguette = $em->getRepository('PJMAppBundle:Item')
+            ->findOneBySlug('baguette')
+            ->getPrix();
 
         $formRechargement = $this->createFormBuilder()
             ->add('montant', 'money', array(
@@ -33,7 +46,7 @@ class BragsController extends Controller
             $resRechargement = json_decode(
                 $this->forward('PJMAppBundle:Consos/Rechargement:getURL', array(
                     'montant' => $montant*100,
-                    'boquette_slug' => $slug
+                    'boquette_slug' => $this->slug
                 ))->getContent(),
                 true
             );
@@ -61,29 +74,82 @@ class BragsController extends Controller
         return $this->render('PJMAppBundle:Consos:brags.html.twig', array(
             'formRechargement' => $formRechargement->createView(),
             'messages' => isset($messages) ? $messages : null,
-            'solde' => $this->getSolde($slug),
-            'prixBaguette' => 0.65,
+            'solde' => $this->getSolde(),
+            'prixBaguette' => $prixBaguette,
             'nbParJour' => 0.5
         ));
     }
 
-    public function getSolde($slug)
+    public function getSolde()
     {
         $em = $this->getDoctrine()->getManager();
 
         $repository = $em->getRepository('PJMAppBundle:Boquette');
-        $boquette = $repository->findOneBySlug($slug);
+        $boquette = $repository->findOneBySlug($this->slug);
 
 
         $repository = $em->getRepository('PJMAppBundle:Compte');
         $compte = $repository->findOneByUserAndBoquette($this->getUser(), $boquette);
 
-        if($compte === null) {
+        if ($compte === null) {
             $solde = 0;
         } else {
             $solde = $compte->showSolde();
         }
 
         return $solde;
+    }
+
+    public function commandeAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $commande = new Historique();
+
+        $form = $this->createForm(new CommandeType(), $commande, array(
+            'action' => $this->generateUrl('pjm_app_consos_brags_commande'),
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $item = $em->getRepository('PJMAppBundle:Item')
+                    ->findOneBySlug('baguette');
+                $commande->setItem($item);
+                $commande->setUser($this->getUser());
+                $commande->setNombre($commande->getNombre()*10);
+                $em->persist($commande);
+                $em->flush($commande);
+
+                $request->getSession()->getFlashBag()->add(
+                    'success',
+                    'Ta commande a été passée. Elle sera validée par le ZiBrag\'s le jour où tu commenceras à pouvoir prendre ton pain. Tu seras notifié.'
+                );
+            } else {
+                $request->getSession()->getFlashBag()->add(
+                    'danger',
+                    'Un problème est survenu lors de ta commande. Réessaye.'
+                );
+
+                $data = $form->getData();
+
+                foreach ($form->getErrors() as $error) {
+                    $request->getSession()->getFlashBag()->add(
+                        'warning',
+                        $error->getMessage()."<br>".$data->getNombre()
+                    );
+                }
+
+            }
+
+            return $this->redirect($this->generateUrl('pjm_app_consos_brags_index'));
+        }
+
+        return $this->render('PJMAppBundle:Consos:Brags/commande.html.twig', array(
+            'form' => $form->createView(),
+            'messages' => isset($messages) ? $messages : null,
+            'nbParJour' => 0.5
+        ));
     }
 }
