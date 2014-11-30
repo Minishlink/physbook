4c133fd775846726cc48c4ec1572158e5cd65f10
Louis Lagrange <lagrange.louis@gmail.com>
2014-11-30 18:50:59 +0100

    debut vacances feriees, passage historique -> commande

---
+++ src/PJM/AppBundle/Entity/Commande.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace PJM\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Commande
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="PJM\AppBundle\Entity\CommandeRepository")
+ */
+class Commande
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="PJM\AppBundle\Entity\Item")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $item;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="PJM\UserBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     * @Assert\DateTime()
+     */
+    private $date;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_debut", type="datetime", nullable=true)
+     * @Assert\DateTime()
+     */
+    private $dateDebut;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_fin", type="datetime", nullable=true)
+     * @Assert\DateTime()
+     */
+    private $dateFin;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="valid", type="boolean", nullable=true)
+     */
+    private $valid;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="nombre", type="smallint")
+     */
+    private $nombre;
+
+    public function __construct()
+    {
+        $this->date = new \DateTime();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Historique
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param integer $nombre
+     * @return Historique
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return integer
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set item
+     *
+     * @param \PJM\AppBundle\Entity\Item $item
+     * @return Historique
+     */
+    public function setItem(\PJM\AppBundle\Entity\Item $item)
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+
+    /**
+     * Get item
+     *
+     * @return \PJM\AppBundle\Entity\Item
+     */
+    public function getItem()
+    {
+        return $this->item;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \PJM\UserBundle\Entity\User $user
+     * @return Historique
+     */
+    public function setUser(\PJM\UserBundle\Entity\User $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \PJM\UserBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set valid
+     *
+     * @param boolean $valid
+     * @return Historique
+     */
+    public function setValid($valid)
+    {
+        $this->valid = $valid;
+
+        return $this;
+    }
+
+    /**
+     * Get valid
+     *
+     * @return boolean
+     */
+    public function getValid()
+    {
+        return $this->valid;
+    }
+
+    /**
+     * Set dateDebut
+     *
+     * @param \DateTime $dateDebut
+     * @return Commande
+     */
+    public function setDateDebut($dateDebut)
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    /**
+     * Get dateDebut
+     *
+     * @return \DateTime
+     */
+    public function getDateDebut()
+    {
+        return $this->dateDebut;
+    }
+
+    /**
+     * Set dateFin
+     *
+     * @param \DateTime $dateFin
+     * @return Commande
+     */
+    public function setDateFin($dateFin)
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    /**
+     * Get dateFin
+     *
+     * @return \DateTime
+     */
+    public function getDateFin()
+    {
+        return $this->dateFin;
+    }
+}
