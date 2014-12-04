f4b08352224a91d0ab0a0b8d2308efcf03221fbc
Louis Lagrange <lagrange.louis@gmail.com>
2014-12-04 02:50:23 +0100

    prendre en compte date commandes

--- src/PJM/AppBundle/Entity/CommandeRepository.php
+++ src/PJM/AppBundle/Entity/CommandeRepository.php
@@ -15,14 +15,14 @@ class CommandeRepository extends EntityRepository
 {
     public function findByUserAndItemSlug(User $user, $item_slug)
     {
-        $query = $this->createQueryBuilder('h')
-                    ->where('h.user = :user')
-                    ->join('h.item', 'i', 'WITH', 'i.slug = :item_slug')
+        $query = $this->createQueryBuilder('c')
+                    ->where('c.user = :user')
+                    ->join('c.item', 'i', 'WITH', 'i.slug = :item_slug')
                     ->setParameters(array(
                         'user' => $user,
                         'item_slug'  => $item_slug,
                     ))
-                    ->orderBy('h.date', 'desc')
+                    ->orderBy('c.date', 'desc')
                     ->getQuery();
 
         try {
@@ -36,12 +36,12 @@ class CommandeRepository extends EntityRepository
 
     public function findByItemSlug($item_slug)
     {
-        $query = $this->createQueryBuilder('h')
-                    ->join('h.item', 'i', 'WITH', 'i.slug = :item_slug')
+        $query = $this->createQueryBuilder('c')
+                    ->join('c.item', 'i', 'WITH', 'i.slug = :item_slug')
                     ->setParameters(array(
                         'item_slug'  => $item_slug,
                     ))
-                    ->orderBy('h.date', 'desc')
+                    ->orderBy('c.date', 'desc')
                     ->getQuery();
 
         try {
@@ -55,15 +55,47 @@ class CommandeRepository extends EntityRepository
 
     public function findByItemSlugAndValid($item_slug, $valid = true)
     {
-        $query = $this->createQueryBuilder('h')
-                    ->where('h.valid = :valid')
-                    ->join('h.item', 'i', 'WITH', 'i.slug = :item_slug')
+        $query = $this->createQueryBuilder('c')
+                    ->where('c.valid = :valid')
+                    ->join('c.item', 'i', 'WITH', 'i.slug = :item_slug')
                     ->setParameters(array(
                         'item_slug'  => $item_slug,
                         'valid' => $valid
                     ))
-                    ->orderBy('h.date', 'desc')
+                    ->orderBy('c.date', 'desc')
                     ->getQuery();
+
+        try {
+            $res = $query->getResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            $res = null;
+        }
+
+        return $res;
+    }
+
+    public function findByItemSlugAndValidAndAtDate($item_slug, $valid, \DateTime $date)
+    {
+        $qb = $this->createQueryBuilder('c')
+            ->where('c.valid = :valid')
+            ->setParameter('valid', $valid)
+            ->andWhere('c.dateDebut <= :dateDebut')
+            ->setParameter('dateDebut', $date->setTime(23,59,59))
+        ;
+
+        if (!$valid) {
+            $qb
+                ->andWhere('c.dateFin >= :dateFin')
+                ->setParameter('dateFin', $date->setTime(23,59,59))
+            ;
+        }
+
+        $query = $qb
+            ->join('c.item', 'i', 'WITH', 'i.slug = :item_slug')
+            ->setParameter('item_slug', $item_slug)
+            ->orderBy('c.date', 'desc')
+            ->getQuery()
+        ;
 
         try {
             $res = $query->getResult();
