5e1bf35f2e0c55b216fbb399d8086bc1d06900dd
Louis Lagrange <lagrange.louis@gmail.com>
2015-01-12 00:51:02 +0100

    continue page admin paniers

--- src/PJM/AppBundle/Controller/Consos/PaniersController.php
+++ src/PJM/AppBundle/Controller/Consos/PaniersController.php
@@ -58,4 +58,80 @@ class PaniersController extends BoquetteController
 
         return $this->render('PJMAppBundle:Consos:Paniers/Admin/index.html.twig');
     }
+
+    // ajout et liste paniers
+    public function listePaniersAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $panier = new Item();
+        $panier->setLibelle('Panier de fruits et légumes');
+        $panier->setBoquette($this->getBoquette());
+        $panier->setSlug($this->itemSlug);
+
+        $form = $this->createForm(new PanierType(), $panier, array(
+            'method' => 'POST',
+            'action' => $this->generateUrl('pjm_app_consos_paniers_admin_listePaniers'),
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                // on enregistre le nouveau panier et on désactive l'ancien
+                $ancienPanier = $this->getCurrentPanier();
+                $ancienPanier->setValid(false);
+                $em->persist($ancienPanier);
+                $em->persist($panier);
+
+                $em->flush();
+
+                $request->getSession()->getFlashBag()->add(
+                    'success',
+                    'Le panier a bien été ajouté.'
+                );
+            } else {
+                $request->getSession()->getFlashBag()->add(
+                    'danger',
+                    'Un problème est survenu lors de l\'ajout du panier. Réessaye.'
+                );
+
+                $data = $form->getData();
+
+                foreach ($form->getErrors() as $error) {
+                    $request->getSession()->getFlashBag()->add(
+                        'warning',
+                        $error->getMessage()
+                    );
+                }
+            }
+
+            return $this->redirect($this->generateUrl('pjm_app_consos_paniers_admin_index'));
+        }
+
+        $datatable = $this->get("pjm.datatable.paniers.liste");
+        $datatable->buildDatatableView();
+
+        return $this->render('PJMAppBundle:Consos:Paniers/Admin/listePaniers.html.twig', array(
+            'form' => $form->createView(),
+            'datatable' => $datatable
+        ));
+    }
+
+    // ajout et liste d'un crédit
+    public function listeCreditsAction(Request $request)
+    {
+        return $this->gestionCredits(
+            $request,
+            'pjm_app_consos_paniers_admin_listeCredits',
+            'pjm_app_consos_paniers_admin_creditsResults',
+            'pjm_app_consos_paniers_admin_index'
+        );
+    }
+
+    // action ajax de rendu de la liste des crédits
+    public function creditsResultsAction()
+    {
+        return $this->creditsResults();
+    }
 }
