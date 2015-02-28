0b153d421c8402c468dd4d66050001cc686c015a
Louis Lagrange <lagrange.louis@gmail.com>
2015-02-28 19:55:54 +0100

    ItemRepository cleaning

--- src/PJM/AppBundle/Entity/ItemRepository.php
+++ src/PJM/AppBundle/Entity/ItemRepository.php
@@ -29,54 +29,6 @@ class ItemRepository extends EntityRepository
         return $res;
     }
 
-    public function findOneBySlugAndValid($slug, $valid)
-    {
-        $query = $this->createQueryBuilder('i')
-                    ->where('i.slug = :slug')
-                    ->andWhere('i.valid = :valid')
-                    ->setParameters(array(
-                        'slug'  => $slug,
-                        'valid'  => $valid,
-                    ))
-                    ->orderBy('i.date', 'desc')
-                    ->getQuery();
-
-        try {
-            $res = $query->getSingleResult();
-        } catch (\Doctrine\Orm\NoResultException $e) {
-            $res = null;
-        }
-
-        return $res;
-    }
-
-    public function findLastOneBySlugAndValid($slug, $valid = true)
-    {
-        $qb = $this->createQueryBuilder('i')
-            ->where('i.slug = :slug')
-            ->setParameter('slug', $slug)
-            ->orderBy('i.date', 'desc')
-            ->setMaxResults(1)
-        ;
-
-        if (isset($valid)) {
-            $qb
-                ->andWhere('i.valid = :valid')
-                ->setParameter('valid', $valid)
-            ;
-        }
-
-        $query = $qb->getQuery();
-
-        try {
-            $res = $query->getSingleResult();
-        } catch (\Doctrine\Orm\NoResultException $e) {
-            $res = null;
-        }
-
-        return $res;
-    }
-
     public function callbackFindBySlug($slug)
     {
         return function($qb) use($slug) {
