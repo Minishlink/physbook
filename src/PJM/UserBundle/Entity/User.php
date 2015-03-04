bc09084672a9e042a36340dcd3d98448863557cb
Louis Lagrange <lagrange.louis@gmail.com>
2015-03-04 16:46:33 +0100

    fix inscription utilisateur

--- src/PJM/UserBundle/Entity/User.php
+++ src/PJM/UserBundle/Entity/User.php
@@ -139,6 +139,8 @@ class User extends BaseUser
     }
 
     public function __construct() {
+        parent::__construct();
+
         $this->responsables = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
