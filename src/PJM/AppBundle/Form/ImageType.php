2005994bcb78885ed35b64d24ae34a0c50b917de
Louis Lagrange <lagrange.louis@gmail.com>
2015-03-10 13:48:14 +0100

    label imageType

--- src/PJM/AppBundle/Form/ImageType.php
+++ src/PJM/AppBundle/Form/ImageType.php
@@ -14,7 +14,9 @@ class ImageType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('file', 'file');
+        $builder->add('file', 'file', array(
+            'label' => 'Fichier (<500ko)',
+        ));
     }
 
     /**
