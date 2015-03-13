13a7ac61d4b3607171938d264576d68e30f38de5
Louis Lagrange <lagrange.louis@gmail.com>
2015-03-13 10:53:29 +0100

    [inbox] fix envoi

--- src/PJM/AppBundle/Controller/InboxController.php
+++ src/PJM/AppBundle/Controller/InboxController.php
@@ -32,9 +32,10 @@ class InboxController extends Controller
          if ($form->isSubmitted()) {
             if ($form->isValid()) {
                 $em = $this->getDoctrine()->getManager();
-                $message->setExpediteur($this->getUser()->getInbox());
+                $message->setExpedition($this->getUser()->getInbox());
                 $em->persist($message);
                 $em->flush();
+
 
                 return $this->redirect($this->generateUrl('pjm_app_inbox_index'));
             }
