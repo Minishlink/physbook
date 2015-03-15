bd53d0eba2546a1a00676126fbd5b5889e1b6cb3
Louis Lagrange <lagrange.louis@gmail.com>
2015-03-15 14:14:24 +0100

    reverse ssl piwik

--- app/Resources/public/js/piwik.js
+++ app/Resources/public/js/piwik.js
@@ -2,7 +2,7 @@ var _paq = _paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-var u="http://analytics.physbook.fr/";
+var u="//analytics.physbook.fr/";
 _paq.push(['setTrackerUrl', u+'piwik.php']);
 _paq.push(['setSiteId', 1]);
 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
