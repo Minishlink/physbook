f5cabaada96d7ee1fa7f187e22bb36e92e4f95a8
Louis Lagrange <lagrange.louis@gmail.com>
2015-03-15 14:10:00 +0100

    sw fix syntax for yui

--- app/Resources/public/js/app.js
+++ app/Resources/public/js/app.js
@@ -2,12 +2,11 @@ window.addEventListener('load', function() {
     var PJMAppBundleDir = "bundles/pjmapp/js/";
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register(window.assetsDir + PJMAppBundleDir + "service-worker.js", { scope: window.assetsDir + PJMAppBundleDir })
-        .then(function(r) {
+        .then(function(sw) {
             console.log('Service worker enregistré');
-        })
-        .catch(function(whut) {
+        }, function (e) {
             console.error('Oups...');
-            console.error(whut);
+            console.error(e);
         });
     } else {
         console.warn('Les service workers ne sont pas encore supportés par ce navigateur.');
