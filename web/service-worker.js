8908067141bb8748d842876b24a8883995a93222
Louis Lagrange <lagrange.louis@gmail.com>
2015-03-15 15:41:12 +0100

    [sw] enlever console.log

--- web/service-worker.js
+++ web/service-worker.js
@@ -2,7 +2,6 @@ var OFFLINE_CACHE = 'offline';
 var OFFLINE_URL = 'html/offline.html';
 
 self.addEventListener('install', function(event) {
-    console.log('install');
     var offlineRequest = new Request(OFFLINE_URL);
     event.waitUntil(
         fetch(offlineRequest).then(function(response) {
@@ -14,32 +13,16 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('fetch');
-  // We only want to call event.respondWith() if this is a GET request for an HTML document.
-  if (event.request.method === 'GET' &&
-      event.request.headers.get('accept').indexOf('text/html') !== -1) {
-    console.log('Handling fetch event for', event.request.url);
-    event.respondWith(
-      fetch(event.request).catch(function(e) {
-        // The catch is only triggered if fetch() throws an exception, which will most likely
-        // happen due to the server being unreachable.
-        // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx range,
-        // the catch() will NOT be called. If you need custom handling for 4xx or 5xx errors, see
-        // https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
+    if (event.request.method === 'GET' &&
+        event.request.headers.get('accept').indexOf('text/html') !== -1) {
 
-        // Normally, fetch() will consult the browser's HTTP caches before attempting a
-        // network request, so in order to trigger offline failure for this sample, we had to
-        // use a cache-busting URL parameter to avoid the cache.
-        console.error('Fetch failed; returning offline page instead.', e);
-        return caches.open(OFFLINE_CACHE).then(function(cache) {
-            return cache.match(OFFLINE_URL);
-        });
-      })
-    );
-  } else { console.log('pas html'); }
-
-  // If our if() condition is false, then this fetch handler won't intercept the request. If there
-  // are any other fetch handlers registered, they will get a chance to call event.respondWith().
-  // If no fetch handlers call event.respondWith(), the request will be handled by the browser
-  // as if there were no service worker involvement.
+        event.respondWith(
+            fetch(event.request).catch(function(e) {
+                // hors ligne
+                return caches.open(OFFLINE_CACHE).then(function(cache) {
+                    return cache.match(OFFLINE_URL);
+                });
+            })
+        );
+  }
 });
