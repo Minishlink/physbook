c46b5c116068e60bec6a49509297ff99070df385
Louis Lagrange <lagrange.louis@gmail.com>
2015-03-22 18:19:43 +0100

    oubli commit

---
+++ src/PJM/AppBundle/Enum/TransactionEnum.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PJM\AppBundle\Enum;
+
+/**
+ * TransactionEnum
+ */
+class TransactionEnum
+{
+    public static function getMoyenPaiementChoices($withValues = false) {
+        $choices = array(
+            'smoney' => "S-Money",
+            'cheque' => "Chèque",
+            'monnaie' => "Monnaie",
+            'initial' => "Solde initial"
+        );
+
+        if ($withValues) {
+            return $choices;
+        }
+
+        return array_keys($choices);
+    }
+}
