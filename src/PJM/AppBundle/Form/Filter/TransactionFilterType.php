1d2f1c6d6121da07ad872c61c1831cf672beff29
Louis Lagrange <lagrange.louis@gmail.com>
2015-03-30 22:25:54 +0200

    [admin] filtre export crédit

---
+++ src/PJM/AppBundle/Form/Filter/TransactionFilterType.php
@@ -0,0 +1,56 @@
+<?php
+namespace PJM\AppBundle\Form\Filter;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Lexik\Bundle\FormFilterBundle\Filter\FilterOperands;
+use Doctrine\ORM\EntityRepository;
+
+class TransactionFilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $enum = new \PJM\AppBundle\Enum\TransactionEnum();
+        $moyenPaiementsChoices = $enum->getMoyenPaiementChoices(true);
+
+        $builder
+            ->add('date', 'filter_date_range', array(
+                'label' => 'Date (intervalle ouvert)',
+                'left_date_options' => array(
+                    'label' => 'De'
+                ),
+                'right_date_options' => array(
+                    'label' => 'A'
+                ),
+            ))
+            ->add('moyenPaiement', 'filter_choice', array(
+                'label' => 'Moyen de paiement',
+                'choices' => $moyenPaiementsChoices
+            ))
+            ->add('export', 'submit', array(
+                'label' => 'Exporter'
+            ))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection'   => false,
+            'validation_groups' => array('filtering')
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'form_pjm_appbundle_transactionfiltertype';
+    }
+}
