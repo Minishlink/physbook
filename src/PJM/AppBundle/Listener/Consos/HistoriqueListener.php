5ba32c2db668e460b5061429647ed14e8a496175
Louis Lagrange <lagrange.louis@gmail.com>
2015-04-07 18:30:32 +0200

    [paniers] annulation possible

--- src/PJM/AppBundle/Listener/Consos/HistoriqueListener.php
+++ src/PJM/AppBundle/Listener/Consos/HistoriqueListener.php
@@ -2,12 +2,21 @@
 
 namespace PJM\AppBundle\Listener\Consos;
 
+use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use PJM\AppBundle\Entity\Historique;
 use PJM\AppBundle\Entity\Compte;
 
-class HistoriqueListener
+class HistoriqueListener implements EventSubscriber
 {
+    public function getSubscribedEvents()
+    {
+        return array(
+            'prePersist',
+            'preRemove',
+        );
+    }
+
     public function prePersist(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
@@ -25,6 +34,29 @@ class HistoriqueListener
                     }
 
                     $compte->debiter($historique->getPrix());
+                    $em->persist($compte);
+                }
+            }
+        }
+    }
+
+    public function preRemove(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $em = $args->getEntityManager();
+
+        if ($entity instanceof Historique) {
+            $historique = $entity;
+            if ($historique->getItem()->getBoquette()->getSlug() != 'cvis' && $historique->getItem()->getBoquette()->getSlug() != 'pians') {
+                if ($historique->getValid()) {
+                    $repository = $em->getRepository('PJMAppBundle:Compte');
+                    $compte = $repository->findOneByUserAndBoquetteSlug($historique->getUser(), $historique->getItem()->getBoquette()->getSlug());
+
+                    if (!isset($compte)) {
+                        $compte = new Compte($historique->getUser(), $historique->getItem()->getBoquette());
+                    }
+
+                    $compte->crediter($historique->getPrix());
                     $em->persist($compte);
                 }
             }
