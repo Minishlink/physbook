5b371dd94634c06fb05f13a22c49cfaafd4d49da
Louis Lagrange <lagrange.louis@gmail.com>
2015-04-17 00:56:32 +0200

    [push] envoi + reception ( manque sync subscription id)

--- web/service-worker.js
+++ web/service-worker.js
@@ -31,3 +31,43 @@ self.addEventListener('fetch', function(event) {
         );
     }
 });
+
+self.addEventListener('push', function(event) {
+    console.log('Notification reçue.', event);
+
+    var data = event.data.json();
+    var title = data.title || "Phy'sbook";
+    var body = data.message || 'Il y a du neuf !';
+    var icon = '/images/favicon/favicon-192x192.png';
+    var tag = 'general';
+
+    event.waitUntil(
+    self.registration.showNotification(title, {
+        body: body,
+        icon: icon,
+        tag: tag
+        })
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('On notification click: ', event.notification.tag);
+    // fix http://crbug.com/463146
+    event.notification.close();
+
+    event.waitUntil(
+        clients.matchAll({
+            type: "window"
+        })
+        .then(function(clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+                var client = clientList[i];
+                if (client.url == '/' && 'focus' in client)
+                    return client.focus();
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
+});
