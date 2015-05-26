96cf956cc0fc2a5b76a3594061d04498c8c06db2
Louis Lagrange <lagrange.louis@gmail.com>
2015-05-27 01:05:30 +0200

    services Group : obtenir la promo 1A etc en cours

---
+++ src/PJM/AppBundle/Services/Group.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PJM\AppBundle\Services;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManager;
+use PJM\UserBundle\Entity\User;
+
+class Group
+{
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Retourne la prom's des 1A actuels
+     * @return string Prom's des 1A actuels
+     */
+    public function getProms1A()
+    {
+        $now = new \DateTime();
+
+        $promsAnnee = (int)(substr($now->format('Y'), 0, 1).substr($now->format('Y'), 2, 3));
+
+        if ($now > new \DateTime(date('Y')."-07-01")) {
+            return $promsAnnee;
+        }
+
+        return $promsAnnee-1;
+    }
+
+    /**
+     * Retourne la prom's des P"N" par rapport aux conscrits actuels (ex. P3 des 213 est 211)
+     * @return string Prom's des PN
+     */
+    public function getPromsPN($n)
+    {
+        return $this->getProms1A()+1-$n;
+    }
+}
