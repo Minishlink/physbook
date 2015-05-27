56cffc8441f5f33e1d94cbea9d7915e0e0d607f2
Louis Lagrange <lagrange.louis@gmail.com>
2015-05-27 17:49:03 +0200

    Markdown parser service

---
+++ src/PJM/AppBundle/Services/Markdown.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PJM\AppBundle\Services;
+
+class Markdown
+{
+    private $parser;
+
+    public function __construct()
+    {
+        $this->parser = new \Parsedown();
+    }
+
+    public function toHtml($text)
+    {
+        $html = $this->parser->text($text);
+
+        return $html;
+    }
+}
