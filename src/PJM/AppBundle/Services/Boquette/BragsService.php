f5f6e6a4e056deca7d8c260417600b310c92f1b2
Louis Lagrange <lagrange.louis@gmail.com>
2015-06-14 22:38:41 +0200

    [insight] use

--- src/PJM/AppBundle/Services/Boquette/BragsService.php
+++ src/PJM/AppBundle/Services/Boquette/BragsService.php
@@ -5,7 +5,7 @@ namespace PJM\AppBundle\Services\Boquette;
 use Doctrine\ORM\EntityManager;
 
 use PJM\UserBundle\Entity\User;
-use PJM\AppBundle\Entity\Compte;
+use PJM\AppBundle\Entity\Item;
 
 class BragsService extends BoquetteService
 {
