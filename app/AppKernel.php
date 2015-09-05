698e78746b0164a31d69c5ad6529ac6466b78687
Louis Lagrange <lagrange.louis@gmail.com>
2015-09-05 13:09:52 +0200

    add staging env

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -33,7 +33,7 @@ class AppKernel extends Kernel
             new PJM\AppBundle\PJMAppBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'staging'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
