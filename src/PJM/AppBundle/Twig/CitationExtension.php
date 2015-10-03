854814a116ca993ff8af8e22e1c53d677fb1b256
Louis Lagrange <lagrange.louis@gmail.com>
2015-10-03 23:16:16 +0200

    fix typo in regex in CitationExtension

--- src/PJM/AppBundle/Twig/CitationExtension.php
+++ src/PJM/AppBundle/Twig/CitationExtension.php
@@ -30,7 +30,7 @@ class CitationExtension extends \Twig_Extension
      */
     public function citationUsersFilter(\Twig_Environment $twig, $texte)
     {
-        preg_match_all("/@[a-zA-Z0-9\-!#]+[bo|li|an|me|ch|cl|ai|ka|pa][0-9]+/", $texte, $usernames);
+        preg_match_all("/@[a-zA-Z0-9-!#]+[bo|li|an|me|ch|cl|ai|ka|pa][0-9]+/", $texte, $usernames);
 
         $repo_user = $this->em->getRepository('PJMAppBundle:User');
 
