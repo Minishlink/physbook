61a8a316ee915124ba71c2709cbf314612de9e56
Louis Lagrange <lagrange.louis@gmail.com>
2015-10-03 23:13:00 +0200

    fix LinkParser (caught wrong links with @)

--- src/PJM/AppBundle/Services/LinkParser.php
+++ src/PJM/AppBundle/Services/LinkParser.php
@@ -16,7 +16,7 @@ class LinkParser
     public function parse($texte)
     {
         return preg_replace_callback(
-            '/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/i',
+            '/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/i',
             function($matches) {
                 return '<a href="'.$matches[0].'">'.$matches[2].substr($matches[4], 0, 10).((strlen($matches[4]) > 10) ? '(...)' : '').'</a>';
             },
