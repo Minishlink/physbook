1080ea280c14f06b4b5a2d23f338aa2c38279822
Louis Lagrange <lagrange.louis@gmail.com>
2015-12-19 13:21:49 +0100

    [trads] fonction obtenant les nums à partir de la fam's
    Pourra être utilisée pour #28

---
+++ src/PJM/AppBundle/Services/Trads.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PJM\AppBundle\Services;
+
+class Trads
+{
+    public function getNums($fams) {
+        return array_map(function($fam) {
+            if ($fam === 'XIII') {
+                return 13;
+            }
+
+            $len = strlen($fam);
+
+            if ($len > 1 && $fam[$len-1] === '!') {
+                return $this->fact((int)substr($fam, 0, $len - 1));
+            }
+
+            if ($len > 2 && substr($fam, -3) === 'bis') {
+                return (int)$fam + 1;
+            }
+
+            return (int)$fam;
+        },  preg_split('/[:#-]/', $fams));
+    }
+
+    private function fact($n) {
+        return $n ? ($n * $this->fact($n-1)) : 1;
+    }
+}
---
+++ src/PJM/AppBundle/Tests/Services/TradsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PJM\AppBundle\Tests;
+
+use PJM\AppBundle\Services\Trads;
+use PJM\AppBundle\Tests\Services\BaseTest;
+
+class TradsTest extends BaseTest
+{
+    /** @var Trads $trads */
+    private $trads;
+
+    public function setUp()
+    {
+        self::bootKernel();
+        $this->trads = static::$kernel->getContainer()->get('pjm.services.trads');
+    }
+
+    /**
+     * @dataProvider famsProvider
+     *
+     * @param string $fams
+     * @param array $expectedNums
+     */
+    public function testGetNums($fams, array $expectedNums)
+    {
+        $this->assertEquals($expectedNums, $this->trads->getNums($fams));
+    }
+
+    public function famsProvider()
+    {
+        return array(
+            array('26#68', array(26, 68)),
+            array('40', array(40)),
+            array('95-157', array(95, 157)),
+            array('XIII-57-142', array(13, 57, 142)),
+            array('5!', array(120)),
+            array('4!-43-92', array(24, 43, 92)),
+            array('168bis', array(169)),
+        );
+    }
+}
