7256f2c17c679531a7fd2bc9c9e1ac8f6111e4fd
Louis Lagrange <lagrange.louis@gmail.com>
2015-12-22 19:45:28 +0100

    [fixtures] ajout des lieux

--- src/PJM/AppBundle/DataFixtures/ORM/LoadBoquetteData.php
+++ src/PJM/AppBundle/DataFixtures/ORM/LoadBoquetteData.php
@@ -15,22 +15,23 @@ class LoadBoquetteData extends AbstractFixture implements OrderedFixtureInterfac
     public function load(ObjectManager $manager)
     {
         $this->loadBoquette($manager, "Phy'sbook", 'physbook', null, 'rouge');
-        $this->loadBoquette($manager, "Pian's", 'pians', 'aeensampian', 'orange');
-        $this->loadBoquette($manager, "C'vis", 'cvis', 'aeensampian', 'rouge');
+        $this->loadBoquette($manager, "Pian's", 'pians', 'aeensampian', 'orange', array('Pians', 'Pian\'s', 'Pian\'ss', 'Pian\'sss', 'Pi'));
+        $this->loadBoquette($manager, "C'vis", 'cvis', 'aeensampian', 'rouge', array('Cvis', 'Cvi\'s', 'Cvi\'ss', 'Cvi\'sss'));
         $this->loadBoquette($manager, "Brag's", 'brags', 'aeensambrags', 'jaune');
         $this->loadBoquette($manager, 'Paniers de fruits et légumes', 'paniers', 'aeensampanier', 'vert');
         $this->loadBoquette($manager, 'AMJE Bordeaux', 'amje-bordeaux', null, 'blanc');
         $this->loadBoquette($manager, 'Asso', 'asso', null, 'bleu');
-        $this->loadBoquette($manager, 'UAI', 'uai', null, 'rose');
+        $this->loadBoquette($manager, 'UAI', 'uai', null, 'rose', array('Gymnase'));
     }
 
-    private function loadBoquette(ObjectManager $manager, $nom, $slug, $caisse, $couleur)
+    private function loadBoquette(ObjectManager $manager, $nom, $slug, $caisse, $couleur, $lieux = null)
     {
         $boquette = new Boquette();
         $boquette->setNom($nom);
         $boquette->setSlug($slug);
         $boquette->setCaisseSMoney($caisse);
         $boquette->setCouleur($couleur);
+        $boquette->setLieux($lieux);
 
         $manager->persist($boquette);
         $manager->flush();
