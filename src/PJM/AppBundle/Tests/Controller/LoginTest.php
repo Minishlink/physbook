f4aa38555d23b3ef27636c330d588f24ec74140c
Louis Lagrange <lagrange.louis@gmail.com>
2016-09-30 17:38:01 +0200

    test buddy

--- src/PJM/AppBundle/Tests/Controller/LoginTest.php
+++ src/PJM/AppBundle/Tests/Controller/LoginTest.php
@@ -12,8 +12,7 @@ class LoginTest extends WebTestCase
         $client->request('GET', '/');
 
         // Make sure we are redirected to the login page
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        var_dump($client->getResponse()->headers);
+        var_dump($client->getResponse());
         $this->assertTrue($client->getResponse()->isRedirect('http://localhost/login'));
 
         $client->followRedirect();
@@ -25,8 +24,9 @@ class LoginTest extends WebTestCase
             '_password' => 'test',
         ));
 
+        var_dump($client->getResponse());
+
         // Login is successful
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
         $this->assertTrue($client->getResponse()->isRedirect('http://localhost/'));
 
         $client->followRedirect();
